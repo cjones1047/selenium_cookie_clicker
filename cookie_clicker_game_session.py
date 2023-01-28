a7ba2d40ba5105020b23166a8d6dfcfc7434d411
cjones1047 <cjmj1047@gmail.com>
2023-01-28 07:58:19 -0600

    buy_best_upgrade method now clicks on the most valuable upgrade available every 5 seconds

--- cookie_clicker_game_session.py
+++ cookie_clicker_game_session.py
@@ -18,6 +18,7 @@ class CookieClickerGameSession:
         self.game_running = True
         self.seconds_since_upgrade = 0
         self.last_upgrade = datetime.now()
+        self.cookie_balance_int = 0
         self.run_game()
 
     def run_game(self):
@@ -35,7 +36,7 @@ class CookieClickerGameSession:
     def buy_best_upgrade(self):
         store_el = self.driver.find_element(by=By.ID, value="store")
         available_item_els = store_el.find_elements(by=By.TAG_NAME, value="div")
-        best_upgrade_dict = dict(el=None, price=0)
+        best_upgrade_dict = dict(el=available_item_els[-1], price=0)
         for item_el in available_item_els[::-1]:
             item_class = item_el.get_attribute("class")
             if not item_class:
@@ -43,5 +44,10 @@ class CookieClickerGameSession:
                 item_title_commas_removed = item_title_el.text.replace(",", "")
                 item_price_str = re.search(r"[-+]?\d*\.?\d+|[-+]?\d+",
                                            item_title_commas_removed).group(0)
-                item_price_float = int(item_price_str)
-                print(item_price_float)
+                item_price_int = int(item_price_str)
+                if item_price_int > best_upgrade_dict["price"]:
+                    best_upgrade_dict = dict(el=item_el, price=item_price_int)
+        cookie_balance_el = self.driver.find_element(by=By.ID, value="money")
+        self.cookie_balance_int = int(cookie_balance_el.text.replace(",", ""))
+        if self.cookie_balance_int > best_upgrade_dict["price"]:
+            best_upgrade_dict["el"].click()
